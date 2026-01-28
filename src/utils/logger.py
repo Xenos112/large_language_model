99ba62cd409aac69b7c39a6b173cee2f16d825eb
Xenos112 <acristyano22@gmail.com>
2026-01-28 16:16:30 +0100

    build a logger

---
+++ src/utils/logger.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from colorama import Fore, Style
+
+
+class Logger:
+    def __init__(self):
+        self.log_level = "INFO"
+
+    def get_date(self):
+        return datetime.now()
+
+    def log(self, message, level="INFO"):
+        if level == "INFO":
+            print(
+                f"{self.get_date()} {Fore.GREEN}{Style.BRIGHT}[INFO]{Style.RESET_ALL} {message}"
+            )
+        elif level == "WARNING":
+            print(
+                f"{self.get_date()} {Fore.YELLOW}{Style.BRIGHT}[WARNING]{Style.RESET_ALL} {message}"
+            )
+        elif level == "ERROR":
+            print(
+                f"{self.get_date()} {Fore.RED}{Style.BRIGHT}[ERROR]{Style.RESET_ALL} {message}"
+            )
+        elif level == "DEBUG":
+            print(
+                f"{self.get_date()} {Fore.BLUE}{Style.BRIGHT}[DEBUG]{Style.RESET_ALL} {message}"
+            )
