a702edfed0d459f9ce6da07769dcb093d461d050
Xenos112 <acristyano22@gmail.com>
2026-01-28 19:45:12 +0100

    feat: add tokenization process using rustbpe

--- src/config/config.py
+++ src/config/config.py
@@ -8,7 +8,12 @@ class Paths:
     DATA_DIR = "./data"
     RAW_DATA_DIR = f"{DATA_DIR}/raw"
     PROCESSED_DATA_DIR = f"{DATA_DIR}/processed"
+    TOKENIZER_FILE = "./src/tokenization/tokenizing.json"
 
 
 class SaveData:
     shard_size = 1024 * 1024 * 1024  # 1GB
+
+
+class ModelConfig:
+    vocab_size = 32000  # just as a start
