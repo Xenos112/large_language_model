3ba62f9e57a5ef1bc12eb86fd35b673ce8434fb1
Xenos112 <acristyano22@gmail.com>
2026-01-28 19:10:03 +0100

    feat: add function to process the data

--- src/config/config.py
+++ src/config/config.py
@@ -8,3 +8,7 @@ class Paths:
     DATA_DIR = "./data"
     RAW_DATA_DIR = f"{DATA_DIR}/raw"
     PROCESSED_DATA_DIR = f"{DATA_DIR}/processed"
+
+
+class SaveData:
+    shard_size = 1024 * 1024 * 1024  # 1GB
